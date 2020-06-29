0ae24b9befbef077251007ebdcf696d59d07546c
Kanji Tanaka <sumireminami@gmail.com>
2020-06-29 23:36:03 +0900

    Refactor to use closure

--- resizer/src/main.rs
+++ resizer/src/main.rs
@@ -17,19 +17,21 @@ fn handler(e: ResizerInput, _: Context) -> Result<(), HandlerError> {
     let client = Client::new();
     client
         .get(&bucket, &key)
-        .map(|buffer| resize(buffer, 480, 270))
+        .map(resize(480, 270))
         .map(|buffer| client.put(&bucket, &new_key, buffer))
         .unwrap();
 
     Ok(())
 }
 
-fn resize(buffer: Vec<u8>, width: u32, height: u32) -> Vec<u8> {
-    let mut out_buffer: Vec<u8> = Vec::new();
-    image::load_from_memory(&buffer)
-        .unwrap()
-        .thumbnail(width, height)
-        .write_to(&mut out_buffer, ImageFormat::Jpeg)
-        .unwrap();
-    out_buffer
+fn resize(width: u32, height: u32) -> Box<dyn Fn(Vec<u8>) -> Vec<u8>> {
+    Box::new(move |buffer| {
+        let mut out_buffer: Vec<u8> = Vec::new();
+        image::load_from_memory(&buffer)
+            .unwrap()
+            .thumbnail(width, height)
+            .write_to(&mut out_buffer, ImageFormat::Jpeg)
+            .unwrap();
+        out_buffer
+    })
 }
